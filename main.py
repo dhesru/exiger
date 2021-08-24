c3e9de7029503a69c150246fc3bdf89387772460
Surendhar <35055664+dhesru@users.noreply.github.com>
2021-08-24 22:57:00 +0800

    Updated codes

--- main.py
+++ main.py
@@ -1,3 +1,52 @@
-import json
+import configparser
+import pandas
+import logging as log
+import requests
 
-print('hey')
+log.basicConfig(level=log.INFO)
+
+parser = configparser.ConfigParser()
+parser.read("file_info.config")
+
+excel_file_input = parser.get('FILE_CONFIG',"input_file")
+excel_file_output = parser.get('FILE_CONFIG',"output_file")
+api = parser.get('API',"url")
+
+df = pandas.read_csv(excel_file_input)
+output = pandas.DataFrame()
+
+def fetch_covid_data(date,iso):
+    final_dict = dict()
+    num_confirmed = 0
+    num_deaths = 0
+    num_recovered = 0
+
+    params = {'date':date,"iso":iso}
+    req = requests.get('https://covid-api.com/api/reports',params=params).json()
+    data = req.get('data',None)
+
+    if data:
+        total_len = len(req.get('data'))
+        for i in range(total_len):
+            num_confirmed = data[i].get('confirmed') + num_confirmed
+            num_deaths = data[i].get('deaths') + num_deaths
+            num_recovered = data[i].get('recovered') + num_recovered
+        final_dict['date'] = date
+        final_dict['iso'] = iso
+        final_dict['num_confirmed'] = num_confirmed
+        final_dict['num_deaths'] = num_deaths
+        final_dict['num_recovered'] = num_recovered
+
+    return final_dict
+
+for index, row in df.iterrows():
+    final_dict = fetch_covid_data(row['date'],row['iso'])
+    if final_dict:
+        output = output.append(final_dict,ignore_index=True)
+        log.info('Data Extracted and stored for %s.', row['iso'])
+    else:
+        log.warning('Data could not be extracted for %s.',row['iso'])
+
+output.to_csv(excel_file_output,index=False)
+log.info('DataFrame is written successfully to Excel Sheet.')
+
